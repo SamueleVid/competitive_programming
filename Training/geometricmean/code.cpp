d5c87e48eb387f9b2cdb2855c85f9b070909be81
SamuLatins <86987754+SamuLatins@users.noreply.github.com>
2024-06-22 20:26:58 +0200

    Update code.cpp

--- Training/geometricmean/code.cpp
+++ Training/geometricmean/code.cpp
@@ -1,85 +1,86 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 
 struct num {
-	// primes has max 7 keys, as the 
-	// inverse primorial of MAXV 1e6 is 7
-	map<int,int> primes;
-	long long value;
-	
-	void pulisci() {
-		value = 1;
-	
-		vector<int> to_erase;
-		for (auto &[p,k] : primes) {
-			k %= 4;
-			for (int j = 0; j < k; j ++) value *= p;
-			if (!k) to_erase.push_back(p);
-		}
-		for (auto p : to_erase) primes.erase(p);
-	}
+    // primes has max 7 keys, as the 
+    // inverse primorial of MAXV 1e6 is 7
+    map<int,int> primes;
+    ll value;
+    
+    void pulisci() {
+        value = 1;
 
-	num(int n) {
-		primes = {};
-		// O(sqrtN)
-		for (int i = 2; i * i <= n; i ++) {
-			while (n % i == 0) {
-				primes[i] ++;
-				n /= i;
-			}
-		}
-		if (n != 1) primes[n] ++;
-		pulisci();
-	}
-	num(map<int,int> primes_) {
-		primes = primes_;
-	}
-	num() {
-		primes = {};
-	}
-	
-	num operator*(num b) {
-		num res(primes);
-		for (auto &[p,k] : b.primes) {
-			res.primes[p] += k;
-		}
-		res.pulisci();
-		return res;
-	}
-	
-	long long inv() {
-		long long res = value;
-		for (auto &[p,k] : primes) {
-			if (k == 1) res *= p * p;
-			if (k == 3) res /= p * p;
-		}
-		return res;
-	}
+        vector<int> to_erase;
+        for (auto &[p,k] : primes) {
+            k %= 4;
+            for (int j = 0; j < k; j ++) value *= p;
+            if (!k) to_erase.push_back(p);
+        }
+        for (auto p : to_erase) primes.erase(p);
+    }
+
+    num(int n) {
+        primes = {};
+        // O(sqrtN)
+        for (int i = 2; i * i <= n; i ++) {
+            while (n % i == 0) {
+                primes[i] ++;
+                n /= i;
+            }
+        }
+        if (n != 1) primes[n] ++;
+        pulisci();
+    }
+    num(map<int,int> primes_) {
+        primes = primes_;
+    }
+    num() {
+        primes = {};
+    }
+    
+    num operator*(num b) {
+        num res(primes);
+        for (auto &[p,k] : b.primes) {
+            res.primes[p] += k;
+        }
+        res.pulisci();
+        return res;
+    }
+    
+    ll inv() {
+        ll res = value;
+        for (auto &[p,k] : primes) {
+            if (k == 1) res *= p * p;
+            if (k == 3) res /= p * p;
+        }
+        return res;
+    }
 };
 
 int main(){
-	ios_base::sync_with_stdio(0); cin.tie(0);
+    ios_base::sync_with_stdio(0); cin.tie(0);
 
-	int N; cin >> N;
-	vector<int> V(N);
-	for (auto &x : V) cin >> x;
+    int N; cin >> N;
+    vector<int> V(N);
+    for (auto &x : V) cin >> x;
+    
+    vector<num> A(N);
+    for (int i = 0; i < N; i ++) A[i] = num(V[i]);
+    
+    ll res = 0;
+    map<ll, ll> m;
+    m[(A[0] * A[1]).inv()] = 1;
+    
+    // O(N^2logN)
+    for (int i = 2; i < N; i ++) {
+        for (int j = i + 1; j < N; j ++) {
+            res += m[(A[i] * A[j]).value];
+        }
+        for (int j = 0; j < i; j ++) {
+            m[(A[i] * A[j]).inv()] ++;
+        }
+    }
 	
-	vector<num> A(N);
-	for (int i = 0; i < N; i ++) A[i] = num(V[i]);
-	
-	long long res = 0;
-	map<long long, long long> m;
-	m[(A[0] * A[1]).inv()] = 1;
-	
-	// O(N^2logN)
-	for (int i = 2; i < N; i ++) {
-		for (int j = i + 1; j < N; j ++) {
-			res += m[(A[i] * A[j]).value];
-		}
-		for (int j = 0; j < i; j ++) {
-			m[(A[i] * A[j]).inv()] ++;
-		}
-	}
-	
-	cout << res << '\n';
+    cout << res << '\n';
 }
