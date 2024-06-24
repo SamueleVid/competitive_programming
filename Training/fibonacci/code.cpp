77f7446502900e90cd79b09154e64ee89c1c9f4e
SamuLatins <86987754+SamuLatins@users.noreply.github.com>
2024-06-24 18:56:48 +0200

    Create code.cpp

---
+++ Training/fibonacci/code.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const int MAXM = 1e9+10;
+map<pair<int, int>, ll> dp;
+map<int, ll> res;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int T; cin >> T;
+    dp[{1, 2}] = 1;
+    
+    for (auto [n, v] : dp) {
+        auto [a, b] = n;
+        if (b + b < MAXM) dp[{b, b + b}] += dp[{a, b}];
+        if (a + b < MAXM) dp[{b, a + b}] += dp[{a, b}];
+    }
+
+    for (auto [n, v] : dp) {
+        auto [a, b] = n;
+        res[b] += v;
+    }
+
+    while (T --) {
+        int M; cin >> M;
+        cout << res[M] << '\n';
+    }
+}
