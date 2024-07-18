c0917f2095cd3b323c1655887843081dd3ed0b42
SamuLatins <86987754+SamuLatins@users.noreply.github.com>
2024-07-18 11:30:24 +0200

    Update code.cpp

--- Training/equal/code.cpp
+++ Training/equal/code.cpp
@@ -10,16 +10,16 @@ void make_all_equal(int N, int Q_add, int Q_compare) {
     
     for (int i = 1; i <= log2(N); i ++) {
         if (N > 256) {
-		    // altrimenti la differenza diventa maggiore di 2^20, ma solo se 
-		    // N > 256 e ci sono abbastanza passaggi da far alzare la diff di tanto
-    		vector<int> alza_bassi;
-    		for (int i = 0; i < N / 2; i ++) alza_bassi.push_back(i);
-    		add(alza_bassi, 1 << 20);
-		}
+            // altrimenti la differenza diventa maggiore di 2^20, ma solo se 
+            // N > 256 e ci sono abbastanza passaggi da far alzare la diff di tanto
+            vector<int> alza_bassi;
+            for (int i = 0; i < N / 2; i ++) alza_bassi.push_back(i);
+            add(alza_bassi, 1 << 20);
+        }
 
         // splitto in casi da due [(A), (B)], A != B, abbasso la differenza
-		// (alzando il minore) con una BSA,
-		// poi quando A = B (e C = D) considero [(A, B), (C, D)] con divide and conquer.
+        // (alzando il minore) con una BSA,
+        // poi quando A = B (e C = D) considero [(A, B), (C, D)] con divide and conquer.
         vector<int> S;
         for (int j = 0; j < N; j += (1 << i)) {
             for (int k = j; k < j + (1 << (i - 1)); k ++) {
@@ -30,7 +30,7 @@ void make_all_equal(int N, int Q_add, int Q_compare) {
             add(S, pw);
         }
 
-        // necessario cuz capitano casi del tipo [A, A + 1] da fixare
+        // necessario cuz capitano casi del tipo [(A), (A + 1)] da fixare
         vector<int> to_add;
         for (int j = 0; j < N; j += (1 << i)) {
             S.clear();
